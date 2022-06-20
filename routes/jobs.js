fa6f3aa9651283ff171c9e7c4ed8a8ee3fa9c54b
Siddhant Singh <sidvz21@gmail.com>
2022-06-21 04:19:22 +0530

    fix tar error

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { update } = require('tar');
 const jobsRouter = express.Router();
 const { getAllJobs, getJob, createJob, updateJob, deleteJob } = require('../controllers/jobs');
 
